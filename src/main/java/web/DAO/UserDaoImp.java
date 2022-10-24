fb6a2cd029b9d3dadc89550c72d7f2abfc24d452
Vino9 <vino9@mail.ru>
2022-10-24 19:06:05 +0700

    Done

--- src/main/java/web/DAO/UserDaoImp.java
+++ src/main/java/web/DAO/UserDaoImp.java
@@ -1,39 +1,38 @@
 package web.DAO;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import web.model.User;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
-@Component
-@Transactional
+@Repository
 public class UserDaoImp implements UserDao {
 
     @PersistenceContext
     private EntityManager entityManager;
-
+    @Transactional
     @Override
     public List<User> printUser() {
         return entityManager.createQuery("select e from User e ", User.class).getResultList();
     }
-
+    @Transactional
     @Override
     public void add(User user) {
         entityManager.persist(user);
     }
-
+    @Transactional
     @Override
     public void deleteById(int id) {
         entityManager.remove(getById(id));
     }
-
+    @Transactional
     @Override
     public void edit(User user) {
         entityManager.merge(user);
     }
-
+    @Transactional
     @Override
     public User getById(int id) {
         return entityManager.find(User.class,id);
--- src/main/java/web/service/UserServiceImp.java
+++ src/main/java/web/service/UserServiceImp.java
@@ -8,37 +8,37 @@ import web.model.User;
 
 import java.util.List;
 @Service
-@Transactional
+
 public class UserServiceImp implements UserService {
 
     private final UserDao userDao;
-
     @Autowired
+
     public UserServiceImp(UserDao userDao) {
         this.userDao = userDao;
     }
-
+    @Transactional
     @Override
     public List<User> printUser() {
         return userDao.printUser();
     }
 
-
+    @Transactional
     @Override
     public void add(User user) {
         userDao.add(user);
     }
-
+    @Transactional
     @Override
     public void deleteById(int id) {
         userDao.deleteById(id);
     }
-
+    @Transactional
     @Override
     public void edit(User user) {
         userDao.edit(user);
     }
-
+    @Transactional
     @Override
     public User getById(int id) {
         return userDao.getById(id);
